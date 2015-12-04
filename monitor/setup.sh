2e87683f9e964548032020b5b7c62143d3c28ccc
Jack Hosemans <jh247247@gmail.com>
2015-12-04 22:16:03 +1100

    Moved flask to debian repos for updates.

--- monitor/setup.sh
+++ monitor/setup.sh
@@ -2,10 +2,8 @@
 
 echo "Installing prerequisites..."
 sudo apt-get update
-sudo apt-get -y install python3 python3-pip mosquitto mongodb
+sudo apt-get -y install python3 python3-pip python3-flask mosquitto mongodb
 
 # I think you can only install 1 thing at a time using pip...
 sudo pip install paho-mqtt
-sudo pip install Flask
 sudo pip install PyMongo
-
