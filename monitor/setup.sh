57bef3bbc9db305ab36030dd5dc26f2662e7c308
Jack Hosemans <jh247247@gmail.com>
2015-12-04 23:49:17 +1100

    Actually made pip use python3

--- monitor/setup.sh
+++ monitor/setup.sh
@@ -5,6 +5,6 @@ sudo apt-get update
 sudo apt-get -y install python3 python3-pip python3-flask mosquitto mongodb
 
 # I think you can only install 1 thing at a time using pip...
-sudo pip install paho-mqtt
-sudo pip install PyMongo
-sudo pip install flask-restful
+sudo pip3 install paho-mqtt
+sudo pip3 install PyMongo
+sudo pip3 install flask-restful
