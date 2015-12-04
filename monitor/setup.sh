7121c6cfcbb29f3c463f5d85f4978c9e920d6aeb
Jack Hosemans <jh247247@gmail.com>
2015-12-04 22:19:59 +1100

    Added flask restful for the REST api.

--- monitor/setup.sh
+++ monitor/setup.sh
@@ -7,3 +7,4 @@ sudo apt-get -y install python3 python3-pip python3-flask mosquitto mongodb
 # I think you can only install 1 thing at a time using pip...
 sudo pip install paho-mqtt
 sudo pip install PyMongo
+sudo pip install flask-restful
