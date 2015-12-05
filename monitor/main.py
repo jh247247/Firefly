7e9fd38bdd9e2463b5d8fe6958ea5bfcfaf3d380
Jack Hosemans <jh247247@gmail.com>
2015-12-05 15:13:32 +1100

    Added new thread for mqtt client to run in.

--- monitor/main.py
+++ monitor/main.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 from flask import Flask
 import flask_restful as fr
-import core
+import core, mqtt
+import threading
 
 app = Flask(__name__)
 api = fr.Api(app)
 
-core.api_setup(api)
-
-if __name__ == '__main__':
+def flask_run():
     app.run(debug=True,
             host='0.0.0.0')
+
+mqtt_thread = threading.Thread(target=mqtt.run, args = ())
+
+if __name__ == '__main__':
+    core.api_setup(api)
+    # TODO: make sure these threads quit when we do?
+    mqtt_thread.start()
+    flask_run()
