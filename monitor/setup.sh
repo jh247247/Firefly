eaed76771409bb5d20fde582854e03bdc6bf5b01
Jack Hosemans <jh247247@gmail.com>
2015-12-05 18:29:14 +1100

    Added avahi dependency for network discovery.

--- monitor/setup.sh
+++ monitor/setup.sh
@@ -2,9 +2,16 @@
 
 echo "Installing prerequisites..."
 sudo apt-get update
-sudo apt-get -y install python3 python3-pip python3-flask mosquitto mongodb
+sudo apt-get -y install python3 python3-pip python3-flask mosquitto \
+     mongodb avahi-daemon avahi-discover libnss-mdns
 
 # I think you can only install 1 thing at a time using pip...
 sudo pip3 install paho-mqtt
 sudo pip3 install PyMongo
 sudo pip3 install flask-restful
+
+# change permission of avahi folder so we can actually write to it
+sudo chmod 755 -R /etc/avahi/services/
+sudo cp mosquitto.service > /etc/avahi/services/mosquitto.service
+sudo cp *.service > /etc/avahi/services/*
+sudo /etc/init.d/avahi-daemon restart
