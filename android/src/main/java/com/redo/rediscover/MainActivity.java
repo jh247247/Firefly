b6ea05fc51d4aa26f665e397c527e941b4d89988
Jack Hosemans <jh247247@gmail.com>
2015-12-08 18:45:33 +1100

    Add butterknife to mainactivity.

--- android/src/main/java/com/redo/rediscover/MainActivity.java
+++ android/src/main/java/com/redo/rediscover/MainActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.net.nsd.NsdServiceInfo;
 import android.util.Log;
+import android.widget.LinearLayout;
+
+import butterknife.ButterKnife;
+import butterknife.Bind;
 
 public class MainActivity extends Activity {
     private static String TAG = "MainActivity";
@@ -12,15 +16,24 @@ public class MainActivity extends Activity {
     private NsdHelper m_nsdHelper;
     private ServiceCallback m_serviceCallback = new ServiceCallback();
 
+    @Bind(R.id.mainLayout) LinearLayout m_mainLayout;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+	ButterKnife.bind(this);
 
 	m_nsdHelper = new NsdHelper(this,m_serviceCallback);
 	m_nsdHelper.startDiscovery();
+    }
+
+    @Override
+    public void onDestroy() {
+	super.onDestroy();
+	ButterKnife.unbind(this);
     }
 
     class ServiceCallback implements NsdHelper.NsdHelperListener {
