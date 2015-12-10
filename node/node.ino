e23a8b696c31530e710fd9c53277da334e8c13f7
Jack Hosemans <jh247247@gmail.com>
2015-12-10 21:05:46 +1100

    Dirty version of connecting node to NRF module. ESP-03 does not have GPIO18, it's actually GPIO16. -.-

--- node/node.ino
+++ node/node.ino
@@ -1,21 +1,21 @@
 /*
- Basic ESP8266 MQTT example
+  Basic ESP8266 MQTT example
 
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
+  This sketch demonstrates the capabilities of the pubsub library in combination
+  with the ESP8266 board/library.
 
- It connects to an MQTT server then:
+  It connects to an MQTT server then:
   - publishes "hello world" to the topic "outTopic" every two seconds
   - subscribes to the topic "inTopic", printing out any messages
     it receives. NB - it assumes the received payloads are strings not binary
   - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
     else switch it off
 
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
+  It will reconnect to the server if the connection is lost using a blocking
+  reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+  achieve the same result without blocking the main loop.
 
- To install the ESP8266 board, (using Arduino 1.6.4+):
+  To install the ESP8266 board, (using Arduino 1.6.4+):
   - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
        http://arduino.esp8266.com/stable/package_esp8266com_index.json
   - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
@@ -23,19 +23,30 @@
 
 */
 
+
+#include <Esp.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <SPI.h>
 #include <RF24.h>
 
 // nrf setup
-RF24 g_radio_nrf(18,15); // TODO: change depending on hardware config.
+RF24 g_radio_nrf(16, 15); // TODO: change depending on hardware config.
+byte address[6] = "TEST1";
+byte mac[6];
+byte rx_packet[6];
+
+// magics.
+#define PRINT_MAC(m) {int d_i; Serial.print(mac[5],HEX);for(d_i = 4; d_i >= 0; d_i--){Serial.print(":");Serial.print(mac[d_i],HEX);}}
+#define WAIT_FOR_PACKET(r,t,to) {int d_start = micros(); while(!r.available()){if(micros() - d_start > t*1000){to=true;break;}}} 
 
 // Update these with values suitable for your network.
 
 const char* ssid = "linksys";
 const char* password = "0000000000";
-const char* mqtt_server = "192.168.1.10";
+const char* mqtt_server = "192.168.1.10"; // todo: resolve mDNS addr.
+
+
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -47,14 +58,30 @@ void setup_wifi();
 void callback(char* topic, byte* payload, unsigned int length);
 
 void setup() {
+  ESP.wdtDisable();
+  
   pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  pinMode(18, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  pinMode(15, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
   g_radio_nrf.begin();
   g_radio_nrf.printDetails();
-  
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+
+  g_radio_nrf.setPayloadSize(sizeof(mac)); // we want to send the mac address so we can confirm.
+  g_radio_nrf.setPALevel(RF24_PA_LOW);
+  g_radio_nrf.setAutoAck(false);
+
+
+
+  g_radio_nrf.openWritingPipe(address);
+  g_radio_nrf.openReadingPipe(1, address);
+
+  g_radio_nrf.startListening();
+
+  WiFi.macAddress(mac);
+  //setup_wifi();
+  //client.setServer(mqtt_server, 1883);
+  //client.setCallback(callback);
 }
 
 void setup_wifi() {
@@ -118,21 +145,51 @@ void reconnect() {
     }
   }
 }
+
+
+
 void loop() {
-
-  if (!client.connected()) {
-    reconnect();
+  yield();
+  
+  bool timeout = false;
+  
+  g_radio_nrf.stopListening();
+  
+  Serial.print("Sending: ");
+  PRINT_MAC(mac);
+  Serial.println(" via NRF radio");
+  if(!g_radio_nrf.write(mac,6,true)){
+    Serial.println("Packet write failed!");
+  } else {
+    Serial.println("Wrote packet!");
   }
-  client.loop();
+  
+  g_radio_nrf.startListening();
+  delay(100);
+//  delay(200);
+//  //WAIT_FOR_PACKET(g_radio_nrf,500,timeout);
+//  if (g_radio_nrf.available()) {
+//    Serial.println("Packet recieved!");
+//    //g_radio_nrf.read(rx_packet, sizeof(rx_packet));
+//  } else {
+//    Serial.println("No packet recieved!");
+//  }
 
-  long now = millis();
-  if (now - lastMsg > 2000) {
-    lastMsg = now;
-    ++value;
-    snprintf (msg, 75, "hello world #%ld", value);
-    Serial.print("Publish message: ");
-    Serial.println(msg);
-    client.publish("outTopic", msg);
-  }
+  //  if (!client.connected()) {
+  //    reconnect();
+  //  }
+  //  client.loop();
+  //
+  //  long now = millis();
+  //  if (now - lastMsg > 2000) {
+  //    lastMsg = now;
+  //    ++value;
+  //    snprintf (msg, 75, "hello world #%ld", value);
+  //    Serial.print("Publish message: ");
+  //    Serial.println(msg);
+  //    client.publish("outTopic", msg);
+  //  }
+
+
 
 }
