de962d5f8194200f3afbe7778ccdf67c55438026
Jack <jh247247@gmail.com>
2015-12-14 11:24:18 +0000

    Added documentation of node api resources.

--- monitor/node.py
+++ monitor/node.py
@@ -7,6 +7,12 @@ import flask_restful as fr
 
 nodes = MongoClient()['redo']['node']
 
+"""
+This class is the api call for HOSTNAME/node/NODE_ID.
+get: Returns the data known for the given node id, 404 if not known.
+put: Puts the data into the database, appends the node_id. Returns the
+     full JSON inserted into the database.
+"""
 class node_res(fr.Resource):
     def get(self, node_id):
         # grab the data from the database, send it back out
@@ -24,15 +30,28 @@ class node_res(fr.Resource):
         # NOTE: will we ever actually put in a new node using the app?
         dat = {"data" : request.form}
         dat["nodeId"] = node_id
+
         nodes.insert_one(dat)
+
+        # TODO: we may need to clean out the mongodb _id field from this...
         return json.dumps(dat,default=json_util.default)
 
+"""
+This class is the api call for HOSTNAME/node.
+Calling this returns a list of all known node IDs, formatted as basic
+JSON.
+Note that as of yet there is no verification of the format, since this
+is straight from the database we can assume that the data will be
+relatively clean.
+"""
 class nodelist_res(fr.Resource):
     def get(self):
         return nodes.distinct('nodeId')
 
 
-
+"""
+This function sets up the API for Flask.
+"""
 def setup(api):
     api.add_resource(nodelist_res,'/node')
     api.add_resource(node_res,'/node/<int:node_id>')
